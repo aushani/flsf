b64420a7c0f6a228a6d2a3cf2574a3dbf1b416af
Arash Ushani <aushani@gmail.com>
2018-04-20 16:23:58 -0400

    wip data extractor

---
+++ cc/app/data/main.cc
@@ -0,0 +1,56 @@
+#include <string.h>
+
+#include <boost/filesystem.hpp>
+#include <boost/format.hpp>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+namespace fs = boost::filesystem;
+
+int main(int argc, char** argv) {
+  printf("Data extractor\n");
+
+  po::options_description desc("Allowed options");
+  desc.add_options()
+    ("tsf-data-dir", po::value<std::string>(), "TSF Data Directory")
+    ("kitti-log-date", po::value<std::string>(), "KITTI data")
+    ("log-num,l", po::value<int>(), "KITTI Log Number")
+    ;
+
+  // Read options
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  std::string home_dir = getenv("HOME");
+  std::string tsf_data_dir = home_dir + "/data/tsf_data";
+  if (vm.count("tsf-data-dir")) {
+    tsf_data_dir = vm["tsf-data-dir"].as<std::string>();
+  } else {
+    printf("Using default tsf data dir: %s\n", tsf_data_dir.c_str());
+  }
+
+  std::string kitti_log_date = "2011_09_26";
+  if (vm.count("kitti-log-date")) {
+    kitti_log_date = vm["kitti-log-date"].as<std::string>();
+  } else {
+    printf("Using default KITTI date: %s\n", kitti_log_date.c_str());
+  }
+
+  int log_num = -1;
+  if (vm.count("log-num")) {
+    log_num = vm["log-num"].as<int>();
+  }
+
+  printf("Using log %d\n", log_num);
+
+  // Check that path exists
+  std::string dir_name = (boost::format("%s_drive_%04d_sync") % kitti_log_date % log_num).str();
+  fs::path base_path = fs::path(tsf_data_dir) / "kittidata" / kitti_log_date / dir_name;
+
+  if (!fs::exists(base_path)) {
+    printf("Cannot read data from %s\n", base_path.c_str());
+  }
+
+  return 0;
+}
